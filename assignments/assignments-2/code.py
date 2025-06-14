9405a3a75a8363098b88d4db59a0605984714f1a
sssharma9268 <sssharma9268@gmail.com>
2025-06-14 10:28:20 -0500

    langraph basics

---
+++ assignments/assignments-2/code.py
@@ -0,0 +1,9 @@
+# 1. you have to create one supervisor node.
+# 2. create one router function
+# 3. create three more node
+# 3.1 llm call (llm node)
+# 3.2 RAG (rag node)
+# 3.3 web crawler(fetch the info in realtime from internet)
+# 4. created one more node after this for validation for generated output --> explore the validation part how to do that
+# 5. if validation going to be failed in that case again go to supervioser node and then supervisor node will again decide what needs to be call next
+# 6. once the validation will pass then only generate the final output
