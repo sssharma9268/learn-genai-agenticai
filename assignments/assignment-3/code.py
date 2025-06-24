e8eb99d71c732db62a9577078a29c1ba8fffcd0f
sssharma9268 <sssharma9268@gmail.com>
2025-06-23 22:21:20 -0500

    latest changes multiagent

---
+++ assignments/assignment-3/code.py
@@ -0,0 +1,48 @@
+# Assignment:
+    
+# AI Travel Agent & Expense Planner(Purpose: Trip planning for any city worldwide with Realtime data.")
+
+# • Real-time weather information
+# • Top attractions and activities
+# • Hotel cost calculation (per day × total days)
+# • Currency conversion to user's native currency
+# • Complete itinerary generation
+# • Total expense calculation
+# • generate a summary of the entire output
+
+# user_input
+#   |
+# search attraction and activity
+# 1. search attracation
+# 2. search restaurant
+# 3. search activity
+# 4. search transportation
+#   |
+# search weather forcasting
+# 1. get current weather
+# 2. get weather forcast
+#   |
+# search hotel costs
+# 1. search hotel
+# 2. estimate the hotel cost
+# 3. budget_range
+#   |
+# calculate total cost
+# 1. add
+# 2. multiply
+# 3. calculated total cost
+# 4. calcualte the daily budget
+#     | 
+# currency_converion
+# 1. get exchnage rate
+# 2. convert currancy
+#     | 
+# Itinery generation
+# 1. get day plan
+# 2. crete full itinery
+#     |
+# create Trip Summary
+#     |
+# Retun complete traval plan
+
+# Note: if you know the OOPS then design this entire system using object and class in modular fashion.
