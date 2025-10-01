faefd2cea6bc8163791c74c207b081cf5c74d2d1
sssharma9268 <sssharma9268@gmail.com>
2025-10-01 19:02:40 +0530

    Autogen Basics

--- assignments/assignment-2/code.py
+++ assignments/assignment-2/code.py
@@ -8,3 +8,4 @@
 # 4. created one more node after this for validation for generated output --> explore the validation part how to do that
 # 5. if validation going to be failed in that case again go to supervioser node and then supervisor node will again decide what needs to be call next
 # 6. once the validation will pass then only generate the final output
+
